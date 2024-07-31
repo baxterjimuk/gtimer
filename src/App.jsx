c3b6700381d39d6e27bdbd76b4818252cd9ffa9b
Baxter Jimuk <baxterjimuk@gmail.com>
2024-08-01 04:07:28 +0800

    fetching done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
 import { useEffect, useState } from "react";
 import supabase from "../utils/supabase";
+import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState([]);
+  /*   const [countries, setCountries] = useState({});
+  
+    async function getCountries() {
+      const { data } = await supabase.from("countries").select().eq('id', '1').single();
+      setCountries(data);
+    }
+  
+    useEffect(() => {
+      getCountries();
+    }, []); */
 
+  const [defaults, setDefaults] = useState({});
   useEffect(() => {
-    getCountries();
-  }, []);
-
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
-  }
+    const getDefaults = async () => {
+      const { data } = await supabase.from('defaults').select().limit(1).single();
+      setDefaults(data);
+    }
+    getDefaults();
+  })
 
   return (
-    <ul>
-      {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
+    // <ul>
+    //   {countries.map((country) => (
+    //     <li key={country.name}>{country.name}</li>
+    //   ))}
+    // </ul>
+    <table>
+      <caption>
+        The defaults as of {(new Date).toLocaleString()}
+      </caption>
+      <thead>
+        <tr>
+          {Object.keys(defaults).map((property, i) => (<th key={i}>{property}</th>))}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {Object.keys(defaults).map((property, i) => (<td key={i}>{defaults[property]}</td>))}
+        </tr>
+        <tr>
+          {Object.keys(defaults).map((property, i) => (<td key={i}>{typeof (defaults[property])}</td>))}
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
